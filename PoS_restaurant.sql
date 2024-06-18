0d804ee9d629282292f861f943376f9a6489e3ad
yonandamayla <yonandamayla555@gmail.com>
2024-06-18 12:01:20 +0700

    jor insert table & add some function

--- PoS_restaurant.sql
+++ PoS_restaurant.sql
@@ -1,8 +1,10 @@
+-- SQLBook: Code
 DROP DATABASE PoS_restaurant;
 
 CREATE DATABASE PoS_restaurant;
 
 USE PoS_restaurant;
+
 -- tabel untuk item yang dititipi oleh supplier
 CREATE TABLE supplier_item (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -58,7 +60,7 @@ CREATE TABLE detail_order (
 CREATE TABLE attendance (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     shift ENUM ('Pagi', 'Sore'),
-    attendace_date DATETIME,
+    attendace_date VARCHAR (100),
     attendance_status ENUM ('Hadir', 'Absen'),
     employee_id INT,
     FOREIGN KEY (employee_id) REFERENCES employee(id)
@@ -84,56 +86,22 @@ CREATE TABLE expense (
 
 
 INSERT INTO supplier_item (id, quantity, price, no_telp, name) VALUES 
-    (001, 100, 4000, 081234567890, 'Roida'),
-    (002, 100, 5000, 081234567891, 'Roziqin'),
-    (003, 100, 6000, 081234567892, 'Aisyah');
+    (300, 20, 4000, 089635247835, 'Roida'),
+    (301, 25, 5000, 089753268632, 'Rozi'),
+    (302, 10, 6000, 089782658735, 'Hainan');
 
 -- menampilkan item dari supplier dan siapa suppliernya
 SELECT * FROM supplier_item;
 
-/*10* = makanan owner
-20* = minuman owner
-30* = sambal owner
-40* = makanan supplier
-50* = minuman supplier*/
+/*10* = makanan dan minuman owner
+13* = makanan dan minuman supplier*/
 INSERT INTO item (id, name, price, supplier_item_id) VALUES 
-    (101, 'Nasi Putih', 4000, NULL),
-    (102, 'Pangsit', 1000, NULL),
-    (103, 'Sayur Bayam', 2000, NULL),
-    (104, 'Sayur Terong', 2000, NULL),
-    (105, 'Sayur Buncis', 2000, NULL),
-    (106, 'Ayam Krispy', 6000, NULL),
-    (107, 'Ikan Tongkol', 2000, NULL),
-    (108, 'Telur Goreng', 3000, NULL),
-    (109, 'Sambal Goreng Tempe', 2000, NULL),
-    (110, 'Tumis Kangkung', 2000, NULL),
-    (111, 'Ikan Lele Krispy', 4000, NULL),
-    (112, 'Ayam Bumbu Kuning', 4000, NULL),
-    (113, 'Mie Goreng', 2000, NULL),
-    (114, 'Tempe Goreng', 1000, NULL),
-    (115, 'Usus', 2000, NULL),
-    (116, 'Perkedel', 2000, NULL),
-    (117, 'Telur Asin', 4000, NULL),
-    (118, 'Telur Balado', 3000, NULL),
-    (119, 'Ayam Kecap', 4000, NULL),
-    (120, 'Tempe / Tahu Kecap', 1000, NULL),
-    (121, 'Sayur Sop', 2000, NULL),
-    (122, 'Sayur Asam', 2000, NULL),
-    (123, 'Sayur Daun Singkong', 2000, NULL),
-    (201, 'Teh Hangat / Es', 3000, NULL),
-    (202, 'Jeruk Hangat / Es', 3000, NULL),
-    (203, 'Hilo Anget / Es', 5000, NULL),
-    (204, 'Nutrisari', 3000, NULL),
-    (205, 'Milo', 6000, NULL),
-    (206, 'Kopi', 3000, NULL),
-    (207, 'Air Mineral', 2000, NULL),
-    (301, 'Sambal Bawang', 1000, NULL),
-    (302, 'Sambal Ijo', 1000, NULL),
-    (303, 'Sambal Tomat', 1000, NULL),
-    (304, 'Sambal Terasi', 1000, NULL),
-    (401, 'Telur Gulung', 4000, 001),
-    (402, 'Empal Sapi', 5000, 002),
-    (501, 'Trily', 6000, 003);
+    (101, 'Ayam Krispy', 6000, NULL),
+    (102, 'Telur Dadar', 3000, NULL),
+    (131, 'Empal Daging', 5000, 300),
+    (132, 'Trili', 6000, 301),
+    (133, 'Telur Gulung', 7000, 302),
+    (103, 'Es Teh', 3000, NULL);
 
 -- menampilkan item yang dijual di warung makan (makanan, minuman, sambal), baik dari supplier maupun buatan sendiri
 SELECT * FROM item;
@@ -151,66 +119,72 @@ SELECT * FROM item WHERE supplier_item_id IS NULL;
 SELECT * FROM item WHERE supplier_item_id IS NOT NULL;
 
 INSERT INTO employee (id, name, no_telp, role, entry_date, salary, gender, address) VALUES
-    (001, 'Mugik Hidayati', 081234567890, 'Owner', '2022-01-01', 3900000, 'Perempuan', 'Jl. Remujung No. 04'),
-    (002, 'Aminin', 081234567891, 'Pembantu', '2022-05-02', 2500000, 'Laki - Laki', 'Jl. Remujung No. 04'),
-    (003, 'Faizah', 081234567892, 'Pembantu', '2022-01-01', 2500000, 'Perempuan', 'Jl. Kembang Turip No. 03'),
-    (004, 'Ilmi', 081234567893, 'Kasir', '2021-01-01', 3000000, 'Perempuan', 'Jl. Remujung No. 04'),
-    (005, 'Sopiah', 081234567894, 'Pelayan', '2021-01-01', 3000000, 'Perempuan', 'Jl. Simbar Menjangan No. 05'),
-    (006, 'Karminah', 081234567895, 'Juru Masak', '2022-01-01', 3900000, 'Perempuan', 'Jl. Raya Singosari No. 06'),
-    (007, 'Siti', 081234567896, 'Juru Masak', '2022-01-01', 3900000, 'Perempuan', 'Jl. Simbar Menjangan No. 08');
+    (001, 'Ilham Maulana', 085876542787, 'Juru Masak', '2022-01-01', 130000, 'Laki - Laki', 'Jl. Senggani No.22'),
+    (002, 'Ayla Zahra', 0896432685423, 'Kasir', '2023-05-05', 100000, 'Perempuan', 'Jl. Remujung Barat No.78'),
+    (003, 'Nurul Hidayah', 0897423864388, 'Pelayan', '2023-11-06', 90000, 'Perempuan', 'Jl. Soekarno No. 12'),
+    (004, 'Sodikin', 0897624384659, 'Pelayan', '2023-12-06', 90000, 'Laki - Laki', 'Jl. Hatta No. 32'),
+    (005, 'Aisyha', 0897643857354, 'Kasir', '2022-12-07', 110000, 'Perempuan', 'Jl. Buah Manggis No. 34');
 
 -- menampilkan data pegawai
 SELECT * FROM employee;
 
 INSERT INTO `order` (id, time, payment_method, total, payment, money_change, employee_id) VALUES
-    (001, '2024-06-06 07:30:00', 'Cash', 80000, 10000, 2000, 004),
-    (002, '2024-06-06 09:00:00', 'Cash', 10000, 10000, NULL, 004),
-    (003, '2022-06-06 10:30:00', 'Qris', 15000, 15000, NULL, 004),
-    (004, '2022-06-06 14:00:00', 'Qris', 10000, 15000, NULL, 004),
-    (005, '2022-06-06 20:30:00', 'Cash', 10000, 15000, 5000, 004),
-    (006, '2022-06-07 12:00:00', 'Qris', 50000, 50000, NULL, 004),
-    (007, '2022-06-07 15:45:00', 'Cash', 40000, 50000, 10000, 004),
-    (008, '2022-06-07 18:55:00', 'Qris', 14000, 14000, NULL, 004),
-    (009, '2022-06-08 08:00:00', 'Qris', 20000, 20000, NULL, 004),
-    (010, '2022-06-08 15:00:00', 'Cash', 23000, 25000, 2000, 004),
-    (011, '2022-06-08 18:00:00', 'Cash', 20000, 25000, 5000, 004),
-    (012, '2022-06-08 20:00:00', 'Qris', 30000, 30000, NULL, 004),
-    (013, '2022-06-09 12:00:00', 'Cash', 10000, 15000, 5000, 004),
-    (014, '2022-06-09 15:00:00', 'Qris', 20000, 20000, NULL, 004),
-    (015, '2022-06-09 18:00:00', 'Cash', 30000, 30000, NULL, 004),
-    (016, '2022-06-09 20:00:00', 'Cash', 20000, 25000, 5000, 004),
-    (017, '2022-06-10 12:00:00', 'Qris', 10000, 15000, 5000, 004),
-    (018, '2022-06-10 15:00:00', 'Cash', 20000, 20000, NULL, 004),
-    (019, '2022-06-10 18:00:00', 'Cash', 30000, 30000, NULL, 004),
-    (020, '2022-06-10 20:00:00', 'Qris', 20000, 25000, 5000, 004);
-
+    (001, '2024-05-01 07:01:00', 'Cash', 30000, 50000, 20000, 002),
+    (002, '2024-05-01 10:01:00', 'Qris', 50000, 50000, NULL, 002),
+    (003, '2022-05-03 11:05:00', 'Cash', 48000, 100000, 52000, 002),
+    (004, '2022-05-04 15:00:20', 'Qris', 45000, 45000, NULL, 002),
+    (005, '2022-05-05 18:00:00', 'Cash', 70000, 100000, 30000, 005),
+    (006, '2022-05-02 18:10:59', 'Qris', 6000, 6000, NULL, 005);
 
 -- menampilkan data order / pesanan dari customer
 SELECT * FROM `order`;
 
+-- menampilkan transaksi yang menggunakan metode pembayaran Qris
+SELECT * FROM `order` WHERE payment_method = 'Qris';
+
+-- menampilkan transaksi yang menggunakan metode pembayaran Cash
+SELECT * FROM `order` WHERE payment_method = 'Cash';
+
+-- menampilkan transaksi berdasarkan id 1
+SELECT * FROM `order` WHERE id = 1;
+
+-- menampilkan transaksi berdasarkan employee id 002
+SELECT * FROM `order` WHERE employee_id = 002;
+
+-- menampilkan transaksi berdasarkan employee id 005
+SELECT * FROM `order` WHERE employee_id = 005;
+
+-- menampilkan item yang paling sering dibeli
+SELECT item_id, COUNT(item_id) as TotalItem FROM detail_order GROUP BY item_id ORDER BY TotalItem DESC LIMIT 1;
+
+-- menghitung order dengan total transaksi terbanyak
+SELECT order_id, SUM(price_unit * quantity) as TotalTransaction FROM detail_order GROUP BY order_id ORDER BY TotalTransaction DESC LIMIT 1;
+
+-- mengurutkan order dengan total transkasaksi terbanyak ke terendah
+SELECT order_id, SUM(price_unit * quantity) as TotalTransaction FROM detail_order GROUP BY order_id ORDER BY TotalTransaction DESC;
+
 INSERT INTO detail_order (id, price_unit, quantity, item_id, order_id) VALUES
-    (001, 4000, 1, 101, 001),
-    (002, 1000, 2, 102, 002),
-    (003, 2000, 1, 103, 003),
-    (004, 2000, 2, 104, 004),
-    (005, 2000, 4, 105, 005),
-    (006, 6000, 1, 106, 006),
-    (007, 2000, 3, 107, 007),
-    (008, 3000, 1, 108, 008),
-    (009, 2000, 1, 109, 009),
-    (010, 2000, 1, 110, 010);
+    (001, 6000, 5, 101, 001),
+    (002, 5000, 10, 102, 002),
+    (003, 6000, 8, 131, 003),
+    (004, 3000, 15, 132, 004),
+    (005, 7000, 10, 133, 001),
+    (006, 3000, 2, 103, 002);
 
 -- menampilkan detail order / detail pesanan dari customer
 SELECT * FROM detail_order;
 
+-- menampilkan total transaksi per hari
+
+-- menampilkan total jumlah transaksi per bulan
+
 INSERT INTO attendance (id, shift, attendace_date, attendance_status, employee_id) VALUES
-    (001, 'Pagi', '2022-06-06 07:30:00', 'Hadir', 004),
-    (002, 'Pagi', '2022-06-06 09:00:00', 'Hadir', 004),
-    (003, 'Pagi', '2022-06-06 10:30:00', 'Hadir', 004),
-    (004, 'Sore', '2022-06-06 12:00:00', 'Hadir', 004),
-    (005, 'Sore', '2022-06-06 12:00:00', 'Hadir', 005),
-    (006, 'Sore', '2022-06-06 12:00:00', 'Hadir', 006),
-    (007, 'Sore', '2022-06-06 12:00:00', 'Absen', 007);
+    (111, '07:00 - 17:00', '2024-05-01','Hadir', 001),
+    (222, '07:00 - 17:00', '2024-05-01','Hadir', 002),
+    (333, '10:00 - 22:00', '2024-05-03','Hadir', 003),
+    (444, '07:00 - 17:00', '2024-05-04','Hadir', 004),
+    (555, '10:00 - 22:00', '2024-05-05','Hadir', 005),
+    (666, '10:00 - 22:00', '2024-05-02','Hadir', 001);
 
 -- menampilkan presensi pegawai
 SELECT * FROM attendance;
@@ -253,17 +227,16 @@ INSERT INTO expense (id, category, total, employee_id, ingredients_id) VALUES
 -- menampilkan pengeluaran
 SELECT * FROM expense;
 
--- fungsi untuk menghitung total pengeluaran
-SELECT SUM(total) FROM expense;
+-- fungsi untuk menghitung total pengeluaran per hari
 
--- fungsi untuk menghitung total pengeluaran berdasarkan kategori
-SELECT category, SUM(total) FROM expense GROUP BY category;
+-- fungsi untuk menghitung total pengeluaran per bulan
 
--- fungsi untuk menampilkan jumlah transaksi per hari (SOLUSI)
-SELECT DATE(time) AS date, COUNT(*) AS total_transaction FROM `order` GROUP BY DATE(time);
+-- perhitungan laba bersih per hari (pendapatan - pengeluaran/ total order - expense)
 
--- fungsi untuk menampilkan total penjualan per bulan (SOLUSI)
-SELECT MONTH(time) AS month, SUM(total) AS total_sales FROM `order` GROUP BY MONTH(time);
+-- menghitung keuntungan dari supplier
+
+-- menghitung keuntungan owner
+
 
 -- fungsi untuk membuat struk pembayaran
 DELIMITER //
@@ -286,8 +259,3 @@ DROP FUNCTION IF EXISTS generate_receipt;
 
 -- menampilkan struk pembayaran untuk order dengan id 1
 SELECT generate_receipt(1);
-
--- jika ingin menambahkan item baru pada order 1 
-INSERT INTO detail_order (order_id, item_id, quantity)
-VALUES (1, 102, 2);
-
